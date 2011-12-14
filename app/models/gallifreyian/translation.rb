c5314749c53180fbd7d47b36ec12d917a7dbd5e5
chatgris <jboyer@af83.com>
2011-12-14 23:50:03 +0100

    add basic validation on Gallifreyian::Translation

--- app/models/gallifreyian/translation.rb
+++ app/models/gallifreyian/translation.rb
@@ -2,7 +2,14 @@
 class Gallifreyian::Translation
   include Mongoid::Document
 
+  # Fields
+  #
   field :language,    type: Symbol
   field :key,         type: String
   field :datum,       type: String
+
+  # Validations
+  #
+  validates :language, presence: true
+  validates :key, presence: true, uniqueness: true
 end
--- spec/models/gallifreyian/translation_spec.rb
+++ spec/models/gallifreyian/translation_spec.rb
@@ -11,4 +11,10 @@ describe Gallifreyian::Translation do
     it { should have_field(:key, :datum).of_type(String) }
     it { should have_field(:language).of_type(Symbol) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:language) }
+    it { should validate_presence_of(:key) }
+    it { should validate_uniqueness_of(:key) }
+  end
 end
