bd432eb253f6d8d05077d9e30879ff3eccd293d9
chatgris <jboyer@af83.com>
2011-12-14 23:43:02 +0100

    model specs

--- spec/models/gallifreyian/translation_spec.rb
+++ spec/models/gallifreyian/translation_spec.rb
@@ -2,8 +2,13 @@
 require 'spec_helper'
 
 describe Gallifreyian::Translation do
-  it 'should be defined' do
-    defined?(Gallifreyian::Translation).should be_true
+
+  it 'should have a valid factory' do
+    Factory(:translation).should be_valid
+  end
+
+  describe 'fields' do
+    it { should have_field(:key, :datum).of_type(String) }
+    it { should have_field(:language).of_type(Symbol) }
   end
 end
-
