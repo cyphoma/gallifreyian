32d7471e5e6a2a37340e442c9bec9337fa55160b
chatgris <jboyer@af83.com>
2011-12-15 14:15:52 +0100

    add validations on datum's type
    Signed-off-by: chatgris <jboyer@af83.com>
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- app/models/gallifreyian/translation.rb
+++ app/models/gallifreyian/translation.rb
@@ -6,10 +6,19 @@ class Gallifreyian::Translation
   #
   field :language,    type: Symbol
   field :key,         type: String
-  field :datum,       type: String
+  field :datum
 
   # Validations
   #
   validates :language, presence: true
   validates :key, presence: true, uniqueness: true
+  validate  :valid_datum?
+
+  private
+
+  def valid_datum?
+    unless self.datum.is_a?(String)
+      errors.add(:datum, :not_a_string)
+    end
+  end
 end
--- spec/models/gallifreyian/translation_spec.rb
+++ spec/models/gallifreyian/translation_spec.rb
@@ -8,7 +8,8 @@ describe Gallifreyian::Translation do
   end
 
   describe 'fields' do
-    it { should have_field(:key, :datum).of_type(String) }
+    it { should have_field(:datum) }
+    it { should have_field(:key).of_type(String) }
     it { should have_field(:language).of_type(Symbol) }
   end
 
@@ -16,5 +17,9 @@ describe Gallifreyian::Translation do
     it { should validate_presence_of(:language) }
     it { should validate_presence_of(:key) }
     it { should validate_uniqueness_of(:key) }
+
+    it "should not be valid" do
+      Factory.build(:translation, datum: ['test']).should_not be_valid
+    end
   end
 end
