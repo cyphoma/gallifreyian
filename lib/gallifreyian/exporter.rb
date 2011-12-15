de6d02afc652e95538cf4bbda2d35eabf3c910be
chatgris <jboyer@af83.com>
2011-12-15 10:59:03 +0100

    add deep hash to the Exporter
    Signed-off-by: chatgris <jboyer@af83.com>
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- lib/gallifreyian/exporter.rb
+++ lib/gallifreyian/exporter.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+require 'i18n/core_ext/hash' # deep_merge
+
 module Gallifreyian
   class Exporter
 
@@ -7,7 +9,31 @@ module Gallifreyian
     end
 
     def run
+      @locales.each do |locale|
+        #all_translations[locale]
+      end
+    end
 
+    protected
+
+    def to_deep_hash(locale)
+      Gallifreyian::Translation.where(language: locale).all.inject({}) do |hash, translation|
+        tmp_hash = {}
+        translation.key.split('.').reverse.each_with_index do |key, index|
+          if index == 0
+            tmp_hash = {:"#{key}" => translation.datum}
+          else
+            tmp_hash = {:"#{key}" => tmp_hash}
+          end
+        end
+        hash.deep_merge!(tmp_hash)
+      end
+    end
+
+    def all_translations
+      @all_translations ||= @locales.inject({}) do |hash, locale|
+        hash.merge(self.to_deep_hash(locale))
+      end
     end
   end
 end
--- spec/lib/gallifreyian/exporter_spec.rb
+++ spec/lib/gallifreyian/exporter_spec.rb
@@ -2,15 +2,23 @@
 require 'spec_helper'
 
 describe Gallifreyian::Exporter do
+  let(:exporter) { Gallifreyian::Exporter.new([:es, :en])}
+
   before do
     (1..3).each do |i|
-      Gallifreyian::Translation.new(key: "en.test#{i}", datum: "This is a test #{i}", language: :en)
+      Gallifreyian::Translation.create(key: "en.test#{i}", datum: "This is a test #{i}", language: :en)
     end
-    Gallifreyian::Exporter.new([:en]).run
+    Gallifreyian::Translation.create(key: "en.test1.nested", datum: "This is a nested test", language: :en)
+    Gallifreyian::Translation.create(key: "es.test1.nested", datum: "This is a nested es test", language: :es)
+    exporter.run
   end
 
   it "should dump a yaml file" do
     YAML.load_file(Rails.root.join('config', 'locales', 'dumps', 'en.yml'))['en']['test1'].should eq
     "This is a test 1"
+  end
+
+  it 'should have all locales' do
+    exporter.send(:all_translations).keys.should eq [:es, :en]
   end
 end
