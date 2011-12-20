e53af06d5c17c8c03f2d55e2abe6c9f0b3252af6
chatgris <jboyer@af83.com>
2011-12-20 17:34:00 +0100

    Can choose layout.
    Signed-off-by: chatgris <jboyer@af83.com>

--- app/controllers/gallifreyian/application_controller.rb
+++ app/controllers/gallifreyian/application_controller.rb
@@ -1,5 +1,12 @@
 # encoding: utf-8
 module Gallifreyian
   class ApplicationController < ActionController::Base
+    layout :choose_layout
+
+    private
+
+    def choose_layout
+      Gallifreyian::Configuration.layout || 'application'
+    end
   end
 end
