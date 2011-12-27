965df8dfc018eb701121d186b570f8e842132a08
chatgris <jboyer@af83.com>
2011-12-27 11:13:54 +0100

    Added js_locales config key.
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/gallifreyian/configuration.rb
+++ lib/gallifreyian/configuration.rb
@@ -4,7 +4,8 @@ module Gallifreyian
   class Configuration
     class << self
 
-      attr_accessor :layout, :main_language, :helpers, :index_name
+      attr_accessor :layout, :main_language, :helpers, :index_name,
+        :js_locales
 
       def configure(&block)
         yield self
--- spec/dummy/config/initializers/gallifreyian.rb
+++ spec/dummy/config/initializers/gallifreyian.rb
@@ -1,8 +1,11 @@
 # encoding: utf-8
-#I18n.backend = Gallifreyian::Store.backend
+#
 Tire::Model::Search.index_prefix "gallyfreyian-#{Rails.env}"
+
 Gallifreyian::Configuration.configure do |config|
   config.main_language = :en
   config.layout = 'application'
+  config.js_locales = true
 end
+
 I18n.backend = I18n::Backend::Chain.new(Gallifreyian::Store.backend, I18n.backend)
