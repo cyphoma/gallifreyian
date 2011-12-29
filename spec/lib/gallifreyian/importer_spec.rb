6ed772d16fefbe0f77c6ce2a5f5adf982942e37a
klacointe <kevinlacointe@gmail.com>
2011-12-29 14:00:50 +0100

    Add failing spec on multiple files import

--- spec/lib/gallifreyian/importer_spec.rb
+++ spec/lib/gallifreyian/importer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe Gallifreyian::Importer do
   before do
-    Gallifreyian::Importer.new([Rails.root.join('config', 'locales', 'es.yml')]).run
+    Gallifreyian::Importer.new([Rails.root.join('config', 'locales', 'es.yml'), Rails.root.join('config', 'locales', 'en.yml')]).run
   end
 
   it 'should have results' do
@@ -18,6 +18,12 @@ describe Gallifreyian::Importer do
   it 'should find translation' do
     Gallifreyian::I18nKey.where(key: 'activerecord.errors.template.body').
       one.translations.where(language: :es).one.datum.should eq 'Se encontraron problemas con los siguientes campos:'
+  end
 
+  it "should find translations for each imported language" do
+    Gallifreyian::I18nKey.where(key: 'hello').
+      one.translations.where(language: :es).one.datum.should eq 'Hola Mundo'
+    Gallifreyian::I18nKey.where(key: 'hello').
+      one.translations.where(language: :en).one.datum.should eq 'Hello World'
   end
 end
