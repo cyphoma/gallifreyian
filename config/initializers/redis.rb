2e435c0558e1df8c0812884e6825ff2ff7d1332e
klacointe <kevinlacointe@gmail.com>
2012-01-05 13:54:30 +0100

    Use application name as additional redis prefix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,7 @@
 # encoding: utf-8
 require 'redis-namespace'
 
-$gallifreyian_store = Redis::Namespace.new("gallifreyian_#{Rails.env}", :redis => Redis.new)
+$gallifreyian_store = Redis::Namespace.new(
+  "#{Rails.application.class.to_s.split("::").first.downcase}_gallifreyian_#{Rails.env}",
+  :redis => Redis.new
+)
