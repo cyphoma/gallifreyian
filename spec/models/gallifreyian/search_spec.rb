c4633d98e7da8f9ecbcc60e75f55ead2274c8dca
chatgris <jboyer@af83.com>
2012-03-28 12:19:45 +0200

    Remove dead specs.
    Signed-off-by: chatgris <jboyer@af83.com>

--- spec/models/gallifreyian/search_spec.rb
+++ spec/models/gallifreyian/search_spec.rb
@@ -4,14 +4,6 @@ require 'spec_helper'
 describe Gallifreyian::Search do
 
   describe 'callback' do
-    it 'should cast true boolean' do
-      Gallifreyian::Search.new(done: 'true').done.should eq true
-    end
-
-    it 'should cast false boolean' do
-      Gallifreyian::Search.new(done: 'false').done.should eq false
-    end
-
     it 'should remove empty string from languages' do
       Gallifreyian::Search.new(languages: ['', '', 'test']).languages.should eq ['test']
     end
